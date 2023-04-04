4f2b04e062ec9c9279ec52e45a8bc4f7bdb7e73d
engadnanhossain <engr.adnanhossain@gmail.com>
2023-04-04 11:22:34 +0600

    Added Product Component

---
+++ src/components/Product/Product.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Product.css'
+
+const Product = (props) => {
+    const {img, name, price, seller, ratings} = props.product;
+    return (
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p className='product-name'>Price: ${price}</p>
+                <p>Seller: {seller}</p>
+                <p>Rating: {ratings} starts</p>
+            </div>
+            <button className='btn-cart'>Add to Cart</button>
+        </div>
+    );
+};
+
+export default Product;
